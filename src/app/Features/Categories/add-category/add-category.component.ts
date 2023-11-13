514bd95f55a3f8079f412afc533d06f25cb41234
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2023-11-13 11:54:38 -0800

    unsubscribed from model

--- src/app/Features/Categories/add-category/add-category.component.ts
+++ src/app/Features/Categories/add-category/add-category.component.ts
@@ -1,35 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category.model';
 import { CategoryService } from '../Services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
 
   model : AddCategoryRequest;
-
+  private addCategorySubscription?: Subscription
   constructor(private CategoryService: CategoryService){
     this.model = {
         name : "",
         urlHandle : ""
     }
   }
-
+  
   onFormSubmit(){
-    this.CategoryService.addCategory(this.model).subscribe({
-
-
+    this.addCategorySubscription = this.CategoryService.addCategory(this.model).subscribe({
+      
+      
       next: (response)=>{
-         console.log('Success')
+        console.log('Success')
       },
       error: (error) => {
         console.log('error')
       },
     })
-
+    
   }
-
+  
+  ngOnDestroy(): void {
+     this.addCategorySubscription?.unsubscribe();
+  }
 }
