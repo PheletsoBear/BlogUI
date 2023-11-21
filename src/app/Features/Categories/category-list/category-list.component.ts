bf0279b81f52a6e84044df4c6eae785d01959c83
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2023-11-21 12:55:47 -0800

    Finished the get all categories feature

--- src/app/Features/Categories/category-list/category-list.component.ts
+++ src/app/Features/Categories/category-list/category-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../Services/category.service';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit{
+
+ categories?:Category[];
+
+  constructor(private categoryService: CategoryService){} //injecting categoryservice
+  ngOnInit(): void {
+      this.categoryService.getAllCategories().subscribe({
+        next: (Response)=>{
+          this.categories = Response;
+          console.log(Response)
+        }
+  });
+  }
 
 }
