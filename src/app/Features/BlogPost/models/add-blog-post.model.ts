f371a8fd628d6375211f1eca072774dc4670f687
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-01-28 23:45:06 -0800

    changes

---
+++ src/app/Features/BlogPost/models/add-blog-post.model.ts
@@ -0,0 +1,14 @@
+export interface AddBlogPost{
+
+    Title: string;
+    ShortDesc: string;
+    Content: string;
+    FeaturedImgUrl: string;
+    UrlHandle: string;
+    PublishDate: Date;
+    Author: string;
+    IsVisible: boolean
+
+
+
+}
---
+++ src/app/Features/BlogPost/models/blog-post.model.ts
@@ -0,0 +1,11 @@
+export interface BlogPost{
+   id: string;
+    title: string;
+    shortDescription: string;
+    content: string;
+    featuredImgUrl: string;
+   urlHandle: string;
+   author: string;
+    publishDate: Date;
+    isVisible: boolean;
+}
