546731e3f941eb07cbd1f6755ea15d90e62f568e
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-01-28 23:44:24 -0800

    manual

---
+++ src/app/Features/BlogPost/blogpostlist/blogpostlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blogpostlist',
+  templateUrl: './blogpostlist.component.html',
+  styleUrls: ['./blogpostlist.component.css']
+})
+export class BlogpostListComponent {
+
+}
