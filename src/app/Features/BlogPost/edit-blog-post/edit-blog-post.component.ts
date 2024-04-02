56a2b2e37d7f8aa58b86d9c6cf9b82fda123bff2
mashalaneevanspheletso@gmail.com <mashalaneevanspheletso@gmail.com>
2024-04-02 05:29:29 +0200

    Still implementing the edit-fuctionality

--- src/app/Features/BlogPost/edit-blog-post/edit-blog-post.component.ts
+++ src/app/Features/BlogPost/edit-blog-post/edit-blog-post.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BlogPostService } from '../services/blog-post.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { MarkdownModule } from 'ngx-markdown';
 import { BlogPost } from '../models/blog-post.model';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../Categories/Services/category.service';
+import { Category } from '../../Categories/models/category.model';
 @Component({
   selector: 'app-edit-blog-post',
   standalone: true,
@@ -13,48 +15,57 @@ import { CommonModule } from '@angular/common';
   styleUrl: './edit-blog-post.component.css',
   imports: [FormsModule, CommonModule, MarkdownModule]
 })
-export class EditBlogPostComponent implements OnInit, OnDestroy{
-  
-  id : string | null = null
+export class EditBlogPostComponent implements OnInit, OnDestroy {
+
+  id: string | null = null
   paramSubscription?: Subscription;
-  model?: BlogPost ;
-  constructor(private route: ActivatedRoute,private BlogPostService: BlogPostService ) {}
+  model?: BlogPost;
+  categories$?: Observable<Category[]>;
+  selectedCategories?: string[];
+
+
+  constructor(private route: ActivatedRoute,
+              private BlogPostService: BlogPostService, 
+              private categoryService: CategoryService) 
+              { }
+
   ngOnInit(): void {
-    
+ this.categories$ = this.categoryService.getAllCategories();
     this.paramSubscription = this.route.paramMap.subscribe({
-      
-      next: (params)=>{
-        
+
+      next: (params) => {
+
         this.id = params.get('id');
-     if(this.id){
-       
-       this.BlogPostService.getBlogPostById(this.id).subscribe({
-         next: (response) =>{
-           this.model = response;
-           
-           
-          },
-          error: (error: any) =>{
-            console.error('Error:', error);
-          }
-          
-        });
-      }
-      
-    },
-    
-  });
-  
-  
-}
-ngOnDestroy(): void {
-  this.paramSubscription?.unsubscribe()
-}
+        if (this.id) {
+
+          this.BlogPostService.getBlogPostById(this.id).subscribe({
+            next: (response) => {
+              this.model = response;
+              this.selectedCategories = response.categories.map(x => x.id);
 
 
-onFormSubmit(){
-  
-}
+            },
+            error: (error: any) => {
+              console.error('Error:', error);
+            }
+
+          });
+        }
+
+      },
+
+    });
+
+
+  }
+  ngOnDestroy(): void {
+    this.paramSubscription?.unsubscribe()
+  }
+
+
+  onFormSubmit() {
+
+  }
 
 
 }
